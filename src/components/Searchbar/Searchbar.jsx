8400d509b112b839ce787f2e4d5d0dd869a3df65
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-12-28 14:13:11 +0200

    SearchBar on hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { SearchHeader, Form, SearchButton, Input } from './Searchbar.styled';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
 
-export class Searchbar extends Component {
-  state = {
-    inputValue: '',
+export const Searchbar = ({ onSubmit }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = e => {
+    setInputValue(e.target.value);
   };
 
-  handleChange = e => {
-    this.setState({ inputValue: e.currentTarget.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state.inputValue);
+    onSubmit(inputValue);
   };
 
-  render() {
-    return (
-      <SearchHeader>
-        <Form onSubmit={this.handleSubmit}>
-          <SearchButton type="submit">
-            <SearchIcon />
-          </SearchButton>
+  return (
+    <SearchHeader>
+      <Form onSubmit={handleSubmit}>
+        <SearchButton type="submit">
+          <SearchIcon />
+        </SearchButton>
 
-          <Input
-            onChange={this.handleChange}
-            value={this.state.inputValue}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="search"
-          />
-        </Form>
-      </SearchHeader>
-    );
-  }
-}
+        <Input
+          onChange={handleChange}
+          value={inputValue}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="search"
+        />
+      </Form>
+    </SearchHeader>
+  );
+};
